81eeb1de49108c20fd7b94123980013fe63a7ad9
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-04-02 03:19:43 +0200

    Signed-off-by: AhmedRoshdy <ahmedroshdy01029@gmail.com>

--- dom.js
+++ dom.js
@@ -1,10 +1,14 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
 
 
 
 // Form submit event
 form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
 
 
 
@@ -28,5 +32,37 @@ function addItem(e){
 
     itemList.appendChild(li) //add ili to list
 
+    
+
+    
+
 
 }
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure ?')){
+            var li =e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+
+    var items =itemList.getElementsByTagName('li');
+
+
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display ='none';
+        }
+
+
+
+    });
+}
+
